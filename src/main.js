cd4e0496acf836173fa59554629ea6ecb72c3d80
et20020223 <et20020223@gmail.com>
2021-06-09 22:07:38 +0800

    新增檔案

--- src/main.js
+++ src/main.js
@@ -1,12 +1,39 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios,axios);
+//允許跨源
+axios.defaults.withCredentials =true;
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach');
+  if (to.meta.requiresAuth) {
+    console.log('請驗證');
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`;
+    const vm = this;
+    axios.post(api).then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        next();
+      } else {
+        alert(response.data.message);
+        next({ path: '/Login' })
+      }
+    });
+  } else {
+    next();
+  }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue'),
+    meta:{requiresAuth:true}
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    /* webpackChunkName: "about"  '../views/About.vue'*/
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '*',
+    redirect:'Login'
+  },
 ]
 
 const router = new VueRouter({
